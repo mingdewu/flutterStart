917dfff8619db5c07c935b457c4d10a93c575dc4
mingdebaba <wmeadal@live.com>
2023-04-15 15:44:09 +0900

    chatapp login

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async{
@@ -79,7 +80,7 @@ class _LoginPageState extends State<LoginPage>{
                     try {
                       // メール/パスワードでユーザー登録
                       final FirebaseAuth auth = FirebaseAuth.instance;
-                      await auth.createUserWithEmailAndPassword(
+                      final result = await auth.createUserWithEmailAndPassword(
                         email: email,
                         password: password,
                       );
@@ -87,7 +88,7 @@ class _LoginPageState extends State<LoginPage>{
                       // チャット画面に遷移＋ログイン画面を破棄
                       await Navigator.of(context).pushReplacement(
                         MaterialPageRoute(builder: (context) {
-                          return ChatPage();
+                          return ChatPage(result.user!);
                         }),
                       );
                     } catch (e) {
@@ -98,7 +99,27 @@ class _LoginPageState extends State<LoginPage>{
                     }
                   },
                 ),
-              )
+              ),
+              Container(
+                  width:double.infinity,
+                  child:OutlinedButton(
+                    child: Text('ログイン'),
+                    onPressed: () async{
+                      try{
+                        final FirebaseAuth auth = FirebaseAuth.instance;
+                        final result =await auth.signInWithEmailAndPassword(email: email, password: password);
+                        await Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context){
+                              return ChatPage(result.user!);
+                        }),
+                        );
+                      } catch(e){
+                        setState(() {
+                          infoText= "ログインに失敗しました:${e.toString()}";
+                        });
+                      }
+                    },
+              ))
             ],
           ),
         ),
@@ -110,6 +131,8 @@ class _LoginPageState extends State<LoginPage>{
 
 // チャット画面用Widget
 class ChatPage extends StatelessWidget {
+  ChatPage(this.user);
+  final User user;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -120,6 +143,7 @@ class ChatPage extends StatelessWidget {
             icon: Icon(Icons.close),
             onPressed: () async {
               // ログイン画面に遷移＋チャット画面を破棄
+              await FirebaseAuth.instance.signOut();
               await Navigator.of(context).pushReplacement(
                 MaterialPageRoute(builder: (context) {
                   return LoginPage();
@@ -129,13 +153,16 @@ class ChatPage extends StatelessWidget {
           ),
         ],
       ),
+      body: Center(
+        child: Text('ログイン情報:${user.email}'),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () async {
           // 投稿画面に遷移
           await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
-              return AddPostPage();
+              return AddPostPage(user);
             }),
           );
         },
@@ -143,9 +170,17 @@ class ChatPage extends StatelessWidget {
     );
   }
 }
+class AddPostPage extends StatefulWidget{
+  AddPostPage(this.user);
+  final  User user;
+  @override
+  _AddPostPageState createState() => _AddPostPageState();
+}
 
 // 投稿画面用Widget
-class AddPostPage extends StatelessWidget {
+class _AddPostPageState  extends State<AddPostPage> {
+
+  String messageText = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -153,12 +188,40 @@ class AddPostPage extends StatelessWidget {
         title: Text('チャット投稿'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('戻る'),
-          onPressed: () {
-            // 1つ前の画面に戻る
-            Navigator.of(context).pop();
-          },
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: "メッセージ"),
+              keyboardType: TextInputType.multiline,
+              maxLines: 3,
+              onChanged: (String value){
+                setState(() {
+                  messageText = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+            child: ElevatedButton(
+             child: Text("投稿"),
+            onPressed: () async{
+               final date =DateTime.now().toLocal().toIso8601String();
+               final email = widget.user.email;
+               await FirebaseFirestore.instance.collection("posts").doc().set({
+            'text': messageText,
+            'email': email,
+            'date': date
+              });
+               Navigator.of(context).pop();
+            },
+            ),
+            )
+            ],
+          ),
         ),
       ),
     );
