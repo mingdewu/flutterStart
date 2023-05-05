eef93630e259dc7d82e9cde8d49193cd603e6ca2
mingdebaba <51104897+mingdewu@users.noreply.github.com>
2023-05-05 17:38:39 +0800

    stepper

--- LifeLine/lib/detail_screen.dart
+++ LifeLine/lib/detail_screen.dart
@@ -13,22 +13,94 @@ class DetailScreen extends StatelessWidget {
         title: Text('Detail Screen'),
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-
-            children: [
-              Text(
-                note.title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(note.content),
-            ],
-          ),
-        ),
+        child: ProjectStepper()
       ),
     );
+  }
+}
+
+class ProjectStepper extends StatefulWidget{
+  const ProjectStepper({super.key});
+  @override
+  _ProjectStepperState createState() => _ProjectStepperState();
+}
+
+class _ProjectStepperState extends State<ProjectStepper>{
+  int _currentStep = 0;
+  StepperType stepperType = StepperType.vertical;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+                child:Stepper(
+                  type: stepperType,
+                  physics: ScrollPhysics(),
+                  currentStep: _currentStep,
+                  onStepTapped: (step) => tapped(step),
+                  onStepContinue: continued,
+                  onStepCancel: cancel,
+                  steps: [
+                    Step(title: new Text("first step"),
+                        content: Column(
+                          children: [
+                            TextFormField(
+                              decoration: InputDecoration(labelText: "Detail 1"),
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(labelText:"Detail 2"),
+                            )
+                          ],
+                        ),
+                      isActive: _currentStep >= 0,
+                      state: _currentStep >=1 ?
+                          StepState.complete :StepState.disabled,
+                    ),
+                    Step(
+                        title: new Text("second step"),
+                        content: Column(
+                          children: [
+                            TextFormField(
+                              decoration: InputDecoration(labelText: "Detail 3"),
+                            ),
+                          ],
+                        ),
+                      isActive: _currentStep >=0,
+                      state: _currentStep>=2?
+                          StepState.complete :StepState.disabled,
+                    )
+                  ],
+                )
+
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.list),
+        onPressed: switchStepsType,
+      ),
+    );
+  }
+  switchStepsType() {
+    setState(() => stepperType == StepperType.vertical
+        ? stepperType = StepperType.horizontal
+        : stepperType = StepperType.vertical);
+  }
+
+  tapped(int step){
+    setState(() => _currentStep = step);
+  }
+
+  continued(){
+    _currentStep < 2 ?
+    setState(() => _currentStep += 1): null;
+  }
+  cancel(){
+    _currentStep > 0 ?
+    setState(() => _currentStep -= 1) : null;
   }
 }
